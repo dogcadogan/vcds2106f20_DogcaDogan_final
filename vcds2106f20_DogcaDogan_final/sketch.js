74df7ba7ad0866610106e8c913952f818c5b3460
dogcadogan <72401043+dogcadogan@users.noreply.github.com>
2021-02-07 21:15:59 +0300

    v2

--- vcds2106f20_DogcaDogan_final/sketch.js
+++ vcds2106f20_DogcaDogan_final/sketch.js
@@ -1,9 +1,24 @@
+/*
+vcds2106f20
+dogca dogan
+final
+data visualization
+
+direction References =https://www.openprocessing.org/sketch/336862
+
+number of star cloudyPercentage
+rotation windDeg
+frameRate windSpeed
+stroke humidity
+*/
+
 let mic;
 let lW;
 let c;
 let a, b;
 let x;
 let rot;
+
 
 function preload() {
 
@@ -18,6 +33,7 @@ function setup() {
   mic = new p5.AudioIn();
   mic.start();
   x=10;
+
 
 
   //fill(255, 150);
@@ -43,39 +59,46 @@ push();
     d = map(humidity, 49, 88, 30, 255);
     e = map(windDeg, 0, 277, 0, 3.0);
 
-    rot= random(map(d,49,88,60,180));
+    rot= random(map(e,0,277,60,180));
 
     fill(d,c);
 
-    frameRate(random(c,a));
+    frameRate(random(a));
 
-
+push();
    //rotate(random(d,-e));
+   translate(width / 2, height / 2);
    rotate(rot);
 
-    square(a , b, c);
+    circle(a , b, c);
 
       noFill();
-      stroke(a,d,e);
-      strokeWeight(windSpeed);
+      stroke(d,0,d,d);
+      strokeWeight(1);
 
+for (let i = 0; i < cloudyPercentage; i+=10) {
 
-          line(x + a, 150 + b, x+40 + a, 20 + b);
-          line(x+40 + a, 20 + b, x+90 + a, 150 + b);
-          line(x+90 + a, 150 + b, x + a, 80 + b);
-          line(x + a, 80 + b, x + 90 + a, 80 + b);
-          line(x + 90 + a, 80 + b, x + a, 150 + b);
+          line(x +i, 150 , x+40+i , 20 );
+          line(x+40+i , 20 , x+90 +i, 150 );
+          line(x+90+i , 150, x +i, 80 );
+          line(x+i , 80 , x + 90 +i, 80 );
+          line(x + 90 +i, 80 , x +i, 150 );
 
+            circle(x +i, 10, c);
+}
 
-
-
+x += 0.5
+if (x > width/2) {
+x = 0;
+}
+pop();
 //star();
   }
 //x = cloudyPercentage += 1;
 
 }
 
-function star() {
+/*function star() {
 
   for (let x = 0; x < 600; x+=90) {
     for (let y = 0; y < 750; y+=60) {
@@ -89,4 +112,4 @@ function star() {
       line(100 + x, 80 + y, 10 + x, 150 + y);
     }
   }
-}
+}*/
